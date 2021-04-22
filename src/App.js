d2cda615360e5a5f64e79b48c7cbd0f5e3173607
oxal-po <wavenpedia@gmail.com>
2021-04-22 20:00:03 +0200

    ajout du brocker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import mqtt from "mqtt";
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { url: "", error: "", captors: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            <h2><label htmlFor="first-name">URL du Brocker</label></h2>
+          </div>
+          <div>
+            <input
+              type="text" id="server"
+              placeholder="Enter your server URI here"
+              onChange={this.urlListener.bind(this)}
+              value={this.state.url}>
+            </input>
+          </div>
+          <p>{this.state.error}</p>
+        </div>
+      </div>
+    );
+  }
+
+  urlListener(event) {
+    console.log(event);
+    let url = event.target.value;
+    this.setState({ url: url, error: "", captors: this.state.captors });
+    try {
+      const client = mqtt.connect(url);
+      this.setUrl(url, client);
+      console.log(this.state.captors);
+    } catch (error) {
+      this.setState({ url: url, error: "url incorrect", captors: this.state.sensors });
+    }
+  }
+
+
+
+  setUrl(url, client) {
+    const captors = [];
+    let getThis = () => {
+      return this;
+    };
+    mqtt.connect(url);
+
+    client.on("connect", function () {
+      client.subscribe("value/#", function (err) {
+        if (err !== null) {
+          console.log(err);
+        }
+      });
+    });
+
+    client.on("message", function (topic, message) {
+      const id = topic.substring(6);
+
+      const obj = JSON.parse(message.toString());
+
+      let captor = captors.find(element => element.id === id);
+      if (typeof captor === "undefined") {
+        captor = { id, ...obj, values: [] };
+        delete captor.value;
+        captors.push(captor);
+      }
+
+      captor.values.unshift(obj.value);
+      captor.values = captor.values.slice(0, 10);
+
+      getThis().setState({ url: getThis().state.url, alert: getThis().state.alert, captors: captors });
+      console.log(captors);
+    });
+  }
 }
 
 export default App;
