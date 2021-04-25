f3ea5864b6341cf4582a10076095dccc78dfb2c1
ma174899 <12031997>
2021-04-25 16:57:42 +0200

    Suppression des imports inutilisé

--- src/entity/button/Button.test.js
+++ src/entity/button/Button.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from "./button";
-import { BrowserRouter, Route } from "react-router-dom";
+import BrowserRouter from "react-router-dom";
 
-it ("render sans crash", function () {
+it ("render sans crash", () => {
     const div = document.createElement("div");
     ReactDOM.render(<BrowserRouter><Button captors={[]}></Button></BrowserRouter>, div)
 });
