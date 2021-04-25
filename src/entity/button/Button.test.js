03885473bf633c31bea5476e0fb7dffb324fc78c
ma174899 <12031997>
2021-04-25 17:29:43 +0200

    Test des Button avec ou sans capteur

--- src/entity/button/Button.test.js
+++ src/entity/button/Button.test.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import Button from "./Button";
 import BrowserRouter from "react-router-dom";
 
+import {
+    render
+} from '@testing-library/react';
 
-it ("render sans crash", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<BrowserRouter><Button captors={[]}></Button></BrowserRouter>, div)
+describe('render sans crash', () => {
+    it("Avec un capteur", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BrowserRouter><Button captors = {
+            {
+                id: "135716",
+                name: "Temperature Bureau N° 28",
+                type: "TEMPERATURE",
+                values: [1, 2, 3]
+            }
+        }></Button></BrowserRouter>, div)
+    });
+
+    it("Sans un capteur", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BrowserRouter><Button></Button></BrowserRouter>, div)
+    });
 });
