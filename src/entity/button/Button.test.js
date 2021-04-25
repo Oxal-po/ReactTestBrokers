d89abfbae0788afb7b322ab4bd49b84380ee00f9
ma174899 <12031997>
2021-04-25 18:17:33 +0200

    Ajout de test pour des mauvais capteurs

--- src/entity/button/Button.test.js
+++ src/entity/button/Button.test.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from "./Button";
-import BrowserRouter from "react-router-dom";
-
-import {
-    render
-} from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
 
 describe('render sans crash', () => {
     it("Avec un capteur", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BrowserRouter><Button captors = {
+            [{
+                id: "135716",
+                name: "Temperature Bureau N° 28",
+                type: "TEMPERATURE",
+                values: [1, 2, 3]
+            }]
+        }></Button></BrowserRouter>, div)
+    });
+
+    it("Sans un capteur", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BrowserRouter><Button></Button></BrowserRouter>, div)
+    });
+
+    it("Avec un mauvais capteur", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(<BrowserRouter><Button captors = {
+            [{
+                palu: "",
+                values: "alo"
+            }]
+        }></Button></BrowserRouter>, div)
+    });
+
+    it("Avec un objet au lieu d'un tableau", () => {
         const div = document.createElement("div");
         ReactDOM.render(<BrowserRouter><Button captors = {
             {
@@ -18,10 +41,5 @@ describe('render sans crash', () => {
                 values: [1, 2, 3]
             }
         }></Button></BrowserRouter>, div)
-    });
-
-    it("Sans un capteur", () => {
-        const div = document.createElement("div");
-        ReactDOM.render(<BrowserRouter><Button></Button></BrowserRouter>, div)
     });
 });
