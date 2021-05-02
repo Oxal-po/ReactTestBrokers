ea414723d9598c8fc952c6c814fa66cddeae6ee7
ma174899 <12031997>
2021-05-02 17:19:10 +0200

    Ajout des tests sur le onClick de button

--- src/entity/button/Button.test.js
+++ src/entity/button/Button.test.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Button from "./Button";
 import { BrowserRouter } from "react-router-dom";
+import { act } from 'react-dom/test-utils';
+  
+let container;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+  
+afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+});
+
 
 describe('render sans crash', () => {
     it("Avec un capteur", () => {
@@ -42,4 +56,31 @@ describe('render sans crash', () => {
             }
         }></Button></BrowserRouter>, div)
     });
+});
+
+
+describe('Handle Click', () => {
+    it('Test click event', () => {
+        act(() => {
+            ReactDOM.render(<BrowserRouter><Button captors = {
+                [{
+                    id: "135716",
+                    name: "Temperature Bureau N° 28",
+                    type: "TEMPERATURE",
+                    values: [1, 2, 3]
+                }]
+            }></Button></BrowserRouter>, container);
+        });
+
+        const button = container.querySelector('button');
+        const label = container.querySelector('h3');
+        expect(label.textContent).toBe('Temperature Bureau N° 28');
+
+        act(() => {
+            button.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+        });
+        
+
+        // TODO: check if route changes and check captor element update
+      });
 });
